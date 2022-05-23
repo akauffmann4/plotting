35c869182fe68ca527d5eeca1b137cc5db243400
akauffmann4 <33843214+akauffmann4@users.noreply.github.com>
2022-05-23 17:02:39 +0200

    test split overlap

--- mainSplit.py
+++ mainSplit.py
@@ -2,11 +2,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot():
-    mean = pd.read_csv("test.csv")
-    variance = pd.read_csv("variance.csv")
-    observations = pd.read_csv("observations.csv")
+    mean = pd.read_csv("testSplitmean.csv")
+    variance = pd.read_csv("testSplitvariance.csv")
+    observations = pd.read_csv("testSplitobservations.csv")
+
     errorfill(mean.domain, mean.value, variance.value)
     plt.scatter(observations.domain, observations.value, color='red', label='observations')
+    plt.grid()
+    plt.legend(loc='upper right')
+    plt.show()
+
+    mean2 = pd.read_csv("testSplit2mean.csv")
+    variance2 = pd.read_csv("testSplit2variance.csv")
+    observations2 = pd.read_csv("testSplit2observations.csv")
+    errorfill(mean2.domain, mean2.value, variance2.value)
+    plt.scatter(observations2.domain, observations2.value, color='red', label='observations')
     plt.grid()
     plt.legend(loc='upper right')
     plt.show()
