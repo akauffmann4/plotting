195d26d54ba921194ea0850cf87bc7028ea7bef6
akauffmann4 <33843214+akauffmann4@users.noreply.github.com>
2022-06-03 15:34:18 +0200

    final pairplots

--- pairplot.py
+++ pairplot.py
@@ -4,12 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 def plot():
-    df = pd.read_csv('gapminder_data.csv')
-    df.head()
-    sns.pairplot(df)
+    df = pd.read_csv('test.csv')
+    g = sns.pairplot(df, hue="type")
     plt.show()
-
-
 
 if __name__ == '__main__':
     plot()
